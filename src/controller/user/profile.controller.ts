62bbffba964af3fe5dc6c4e65bb41f5c68350dab
mussaddiqmahmood7 <mussaddiqmahmood7@gmail.com>
2025-06-13 11:22:40 +0500

    profile update route added

--- src/controller/user/profile.controller.ts
+++ src/controller/user/profile.controller.ts
@@ -3,20 +3,46 @@ import { asyncHandler } from "../../lib/asyncHandler.js";
 import { ErrorResponse } from "../../lib/errorResponse.js";
 import { User } from "../../models/user.model.js";
 import { APIResponse } from "../../lib/apiResponse.js";
+import { validateData } from "../../lib/validateData.js";
 
-export const ProfileController = asyncHandler(async (req: Request, res: Response) => {
-  const user = req.body.user;
-  if (!user) {
-    throw new ErrorResponse(403, "you are not authorized");
-  }
+interface ProfileUpdateProps {
+  firstName: string;
+  lastName: string;
+}
 
-  const returnUser = await User.findById(user._id).select(
-    "-password -__v -forgotToken"
-  );
+export const GetProfileController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const user = req.body.user;
+    if (!user) {
+      throw new ErrorResponse(403, "you are not authorized");
+    }
 
-  res
-    .status(200)
-    .json(
-      new APIResponse(200, "Profile data fetched successfully", returnUser)
+    const returnUser = await User.findById(user._id).select(
+      "-password -__v -forgotToken"
     );
-});
+
+    res
+      .status(200)
+      .json(
+        new APIResponse(200, "Profile data fetched successfully", returnUser)
+      );
+  }
+);
+
+export const UpdateProfileController = asyncHandler(
+  async (req: Request, res: Response) => {
+    validateData(req.body, ["firstName", "lastName"]);
+    const user = req.body.user;
+    const { firstName, lastName } = req.body as ProfileUpdateProps;
+
+    user.firstName = firstName;
+    user.lastName = lastName;
+    await user.save();
+    
+    const returnUser = await User.findById(user._id).select('-forgotToken -refreshToken -password -_id -__v')
+
+    res
+      .status(201)
+      .json(new APIResponse(201, "Profile Data Updated Successfully",returnUser));
+  }
+);
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from "express";
 import { APIResponse } from "../lib/apiResponse.js";
 import { VerifyAccessTokenMiddleware } from "../middleware/verify-access-token.middleware.js";
-import { ProfileController } from "../controller/user/profile.controller.js";
+import { GetProfileController } from "../controller/user/profile.controller.js";
 
 const router = express.Router();
 
 router.get("/", (_: Request, res: Response) => {
   res.status(200).json(new APIResponse(200, "auth service is working", {}));
 });
-router.get("/profile", VerifyAccessTokenMiddleware, ProfileController);
+router.get("/profile", VerifyAccessTokenMiddleware, GetProfileController);
+router.post("/profile", VerifyAccessTokenMiddleware, GetProfileController);
 
 export const userRouter = router;
